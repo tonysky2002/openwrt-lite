20cfb56eae4079b2b0c759ea70bf406bfef5c08d
tonysky2002 <tonysky2002@gmail.com>
2025-09-14 14:21:52 +0800

    Update 06-custom.sh

--- openwrt/scripts/06-custom.sh
+++ openwrt/scripts/06-custom.sh
@@ -10,3 +10,15 @@ git clone https://$github/sbwml/luci-app-openlist2 package/new/openlist
 # lrzsz - add patched package
 rm -rf feeds/packages/utils/lrzsz
 git clone https://$github/sbwml/packages_utils_lrzsz package/new/lrzsz
+
+# 在编译 OpenWrt 固件时，预设 PPPoE 拨号信息
+
+ZZZ="package/new/default-settings/default/zzz-default-settings"
+
+sed -i '/exit 0/i \
+uci set network.wan.proto="pppoe"\n\
+uci set network.wan.ifname="eth0"\n\
+uci set network.wan.username="21121876459"\n\
+uci set network.wan.password="139220"\n\
+uci commit network\n\
+' $ZZZ
